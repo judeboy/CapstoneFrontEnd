a0ad31b4b63fc4a1b9cbe66854864d8dab40d5cb
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-17 17:56:28 -0600

    stupid trello thing

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,19 +1,7 @@
 import React, { Component } from 'react';
-// import {
-//   Route,
-//   Link,
-//   BrowserRouter as Router,
-// } from 'react-router-dom'
-// import PropTypes from 'prop-types';
-
-import { DragDropContext } from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
-
+import Board from 'react-trello'
 import './App.css';
-// import Header from './Components/Header';
-// import Menu from './Components/Menu';
-import TrelloColumn from './Components/TrelloColumn';
-import Card from './Components/Card'
+
 
 
 
@@ -23,82 +11,78 @@ class App extends Component {
     this.state = {
       greeting: 'Hello',
       mounted:false,
-      // programs: {},
-      // urls: {},
-      type: 'DOD',
-      matchingPrograms: [],
+      programs: {},
+      urls: {},
+      data: {
+      lanes: [
+        {
+          id: 'lane1',
+          title: 'Planned Tasks',
+          label: '2/2',
+          cards: [
+            {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
+            {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
+          ]
+        },
+        {
+          id: 'lane2',
+          title: 'Completed',
+          label: '0/0',
+          cards: []
+        }
+      ]
+    }
     }
   }
+
 
   async componentDidMount() {
     const response = await fetch('https://judahhh.herokuapp.com/')
     const json = await response.json()
-
-    const matchingPrograms = json.filter(program => program.AgencyShort === this.state.type);
-
-    // console.log('matchingPrograms innit : ', matchingPrograms);
-    this.setState({ matchingPrograms });
-    // this.organizeProgs(json);
-    // this.organizeURLs(json)
+    this.organizeProgs(json);
+    this.organizeURLs(json)
     this.setState({
       mounted:true
     })
   }
 
-  // organizeURLs(json){
-  //   let obj = {}
-  //   let urls = json
-  //   for (let i in urls){
-  //     if (!obj[urls[i].AgencyShort]){
-  //       obj[urls[i].AgencyShort] = [urls[i].WebURL]
-  //     } else {
-  //       obj[urls[i].AgencyShort].push(urls[i].WebURL)
-  //       this.setState({urls: obj})
-  //     }
-  //   }
-  // }
-  // organizeProgs(json){
-  //   // console.log('organizeProgs');
-  //   let obj = {}
-  //   let progs = json
-  //   // console.log("progs", progs);
-  //   for (let i in progs){
-  //     // console.log("i", progs[i].AgencyShort, progs[i].ProgTitle);
-  //     if (!obj[progs[i].AgencyShort]){
-  //       obj[progs[i].AgencyShort] = [progs[i].ProgTitle]
-  //       // obj[progs[i].AgencyShort] = [progs[i].ProgTitle]
-  //     } else {
-  //       obj[progs[i].AgencyShort].push(progs[i].ProgTitle)
-  //       this.setState({programs: obj})
-  //     }
-  //   }
-  //   // console.log("function Object", this.state.programs.DOD)
-  // }
+  organizeURLs(json){
+    let obj = {}
+    let urls = json
+    for (let i in urls){
+      if (!obj[urls[i].AgencyShort]){
+        obj[urls[i].AgencyShort] = [urls[i].WebURL]
+      } else {
+        obj[urls[i].AgencyShort].push(urls[i].WebURL)
+        this.setState({urls: obj})
+      }
+    }
+  }
+  organizeProgs(json){
+    // console.log('organizeProgs');
+    let obj = {}
+    let progs = json
+    // console.log("progs", progs);
+    for (let i in progs){
+      // console.log("i", progs[i].AgencyShort, progs[i].ProgTitle);
+      if (!obj[progs[i].AgencyShort]){
+        obj[progs[i].AgencyShort] = [progs[i].ProgTitle]
+        // obj[progs[i].AgencyShort] = [progs[i].ProgTitle]
+      } else {
+        obj[progs[i].AgencyShort].push(progs[i].ProgTitle)
+        this.setState({programs: obj})
+      }
+    }
+    // console.log("function Object", this.state.programs.DOD)
+  }
 
-  renderList = (number, matchingPrograms) => {
-    var piece = null;
-    if(this.props.cardPosition === number){
-        piece = <Card />
-    }
-    // console.log('matchingPrograms', matchingPrograms);
-    return (
-      <TrelloColumn number={number}>
-          {matchingPrograms ? matchingPrograms.map(program => <Card {...program} />) : null}
-      </TrelloColumn>
-    )
-    }
 
   render(){
     return(
-      <div id='containerOfColumns'>
-          {this.renderList('one', this.state.matchingPrograms)}
-          {this.renderList('two')}
-          {this.renderList('three')}
-          {this.renderList('four')}
-      </div>
+      <Board data={this.state.programs}/>
     )
   }
 
 }; //App Component
 
-export default DragDropContext(HTML5Backend)(App)
+export default App

de1fb3aa1ddd4d741f583c2fdb19192d62675ada
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-17 17:21:32 -0600

    single card will work, after mapping over data it wont

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -83,7 +83,7 @@ class App extends Component {
     // console.log('matchingPrograms', matchingPrograms);
     return (
       <TrelloColumn number={number}>
-          {matchingPrograms ? matchingPrograms.map(program => <Card {...program} />) : null}
+          {piece}
       </TrelloColumn>
     )
     }

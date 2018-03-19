8e0cf08c37e2b92d5f1cd147b521195e4ef3cfeb
Judah Martin Trimmer <jutr4020@colorado.edu>
2018-03-19 13:25:44 -0600

    one div dragging

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,88 +1,38 @@
-import React, { Component } from 'react';
-import Board from 'react-trello'
-import './App.css';
 
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Draggable, {DraggableCore} from 'react-draggable';
 
 
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      greeting: 'Hello',
-      mounted:false,
-      programs: {},
-      urls: {},
-      data: {
-      lanes: [
-        {
-          id: 'lane1',
-          title: 'Planned Tasks',
-          label: '2/2',
-          cards: [
-            {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
-            {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
-          ]
-        },
-        {
-          id: 'lane2',
-          title: 'Completed',
-          label: '0/0',
-          cards: []
-        }
-      ]
-    }
-    }
+  // handleStart = (event, ui) => {
+  //   console.log('Event: ', event);
+  //   console.log('Position: ', ui.position);
+  // }
+  //
+  // handleDrag = (event, ui) => {
+  //   console.log('Event: ', event);
+  //   console.log('Position: ', ui.position);
+  // }
+  //
+  //
+  // handleStop = (event, ui) => {
+  //   console.log('Event: ', event);
+  //   console.log('Position: ', ui.position);
+  // }
+
+
+  render() {
+    return (
+      <Draggable>
+        <div className='draggableDiv'>
+          <div className="handle">Drag from here</div>
+          <div>This readme is really dragging on...</div>
+        </div>
+      </Draggable>
+    );
   }
-
-
-  async componentDidMount() {
-    const response = await fetch('https://judahhh.herokuapp.com/')
-    const json = await response.json()
-    this.organizeProgs(json);
-    this.organizeURLs(json)
-    this.setState({
-      mounted:true
-    })
-  }
-
-  organizeURLs(json){
-    let obj = {}
-    let urls = json
-    for (let i in urls){
-      if (!obj[urls[i].AgencyShort]){
-        obj[urls[i].AgencyShort] = [urls[i].WebURL]
-      } else {
-        obj[urls[i].AgencyShort].push(urls[i].WebURL)
-        this.setState({urls: obj})
-      }
-    }
-  }
-  organizeProgs(json){
-    // console.log('organizeProgs');
-    let obj = {}
-    let progs = json
-    // console.log("progs", progs);
-    for (let i in progs){
-      // console.log("i", progs[i].AgencyShort, progs[i].ProgTitle);
-      if (!obj[progs[i].AgencyShort]){
-        obj[progs[i].AgencyShort] = [progs[i].ProgTitle]
-        // obj[progs[i].AgencyShort] = [progs[i].ProgTitle]
-      } else {
-        obj[progs[i].AgencyShort].push(progs[i].ProgTitle)
-        this.setState({programs: obj})
-      }
-    }
-    // console.log("function Object", this.state.programs.DOD)
-  }
-
-
-  render(){
-    return(
-      <Board data={this.state.programs}/>
-    )
-  }
-
-}; //App Component
+}
 
 export default App
--- capstone/src/index.js
+++ capstone/src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './App';
-import {observe, cardPosition} from './Components/Game'
+
 
 
 const rootel =  document.getElementById('root')
 
-observe((cardPosition)=>{
-    ReactDOM.render(<App cardPosition={cardPosition}/>, rootel);
-})
+window.onload = () => {
+  ReactDOM.render(<App/>, rootel);
+};
 
 registerServiceWorker();
